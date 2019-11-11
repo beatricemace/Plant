7b6cbde370652876f0316deb5793f1962aacc1ed
Béatrice Macé <beatrice.mace@yahoo.fr>
2019-11-12 00:26:40 +0100

    les problèmes que je rencontre : mon but pour l'instant : naviguer entre deux fragments pour ca, j'ai crée 2 fragments dans le layout de la resource : le welcome; et le addingplant sauf que pour le addingplant, il y a un probleme concernant le MainActivity (je ne sais pas où placer le code du private fun addInformation(view: View)
    il a également des erreurs dans le AddingPlant dans le dossier java du code 
    
    j'aimerai d'abord d'abord commencer par régler ces problèmes et etre sur que mes 2 fragments fonctionnent bien avant de commencer la naviguation

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.myapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.example.myapplication.databinding.ActivityMainBinding
 
@@ -11,7 +17,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         // setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addName(it)
+        }
     }
 
+    private fun addInformation(view: View){
+        val editText = findViewById<EditText>(R.id.name_edit)
+        val nameTextView = findViewById<TextView>(R.id.name_text)
+
+        nameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nameTextView.visibility = View.VISIBLE
+
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
 }
 
