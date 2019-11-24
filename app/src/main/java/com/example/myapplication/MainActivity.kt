25161a3e6d9f8f4f34604ee89f81fae28d5b7d21
Béatrice Macé <beatrice.mace@yahoo.fr>
2019-11-24 18:45:34 +0100

    Open the creation form

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.example.myapplication
 
-import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.example.myapplication.databinding.ActivityMainBinding
 
@@ -15,25 +11,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        // setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.done_button).setOnClickListener {
-            addName(it)
-        }
+        setContentView(R.layout.activity_main)
     }
 
-    private fun addInformation(view: View){
-        val editText = findViewById<EditText>(R.id.name_edit)
-        val nameTextView = findViewById<TextView>(R.id.name_text)
-
-        nameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nameTextView.visibility = View.VISIBLE
-
-
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    // Open the form to add a new plant
+    fun openCreatePlantForm(view: View) {
+        val intent = Intent(this, CreatePlantActivity::class.java)
+        startActivity(intent)
     }
 }
 
